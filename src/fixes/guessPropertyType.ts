37957e2ba93c0d8e40ba17f34fb7807957c474a4
Santo Pfingsten <core@lusito.info>
2023-09-13 23:58:07 +0200

    ignore non-existing storage constants

--- src/fixes/guessPropertyType.ts
+++ src/fixes/guessPropertyType.ts
@@ -8,7 +8,7 @@ function fixPropertyType(original: any) {
     if (!original.type) {
         const prop = original as SchemaProperty;
         if ("choices" in prop) prop.type = "choices";
-        else if ("properties" in prop) prop.type = "object";
+        else if ("properties" in prop && prop.properties) prop.type = "object";
         else if ("value" in prop) prop.type = "value";
         else if ("$ref" in prop) prop.type = "ref";
         else throw new Error(`Could not guess type for ${JSON.stringify(original)}`);
